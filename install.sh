4766f41f23d448c4d27203d4e6f7c975c83e40c1
Karel van Klink <karel@deob.us>
2019-11-17 16:31:52 +0100

    update install script add play_file method to radio_controller

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 $CURRENT_DIR='`pwd`'
 
-if [ -f $CURRENT_DIR/pifm]; then
-	echo ""
+if [ -f /usr/bin/ffmpeg]; then
+	if [ -f $CURRENT_DIR/pifm]; then
+	  echo ""
+  else
+    echo "Grabbing pifm..."
+
+    echo `wget http://omattos.com/pifm.tar.gz`
+    echo `tar -xzf pifm.tar.gz`
+    echo `rm pifm.tar.gz pifm.c PiFm.pyc sound.wav left_right.wav`
+  fi
+
+  echo "Installing gem bundle"
+
+  echo `bundle install --deployment`
+
 else
-	echo "Grabbing pifm..."
-
-	echo `wget http://omattos.com/pifm.tar.gz`
-	echo `tar -xzf pifm.tar.gz`
-	echo `rm pifm.tar.gz pifm.c PiFm.pyc sound.wav left_right.wav`
+	echo "ffmpeg has to be installed first. Cancelling install..."
 fi
-
-echo "Installing bundle"
-
-echo `bundle install --deployment`

966a3ffb721be591c80f8f262d1caff3a431db54
Karel van Klink <karel@deob.us>
2019-11-18 18:11:03 +0100

    remove namespace keyword just so yard will work

--- server.rb
+++ server.rb
@@ -13,78 +13,74 @@ class RadioBaggaBackend < Sinatra::Base
     content_type :json
   end
 
-  # Version 1 of the API can be reached at /v1/
-  namespace '/v1' do
+  # GET request to `/v1/queue` will result in the current queue
+  # Optionally takes 2 arguments: start and count
+  #
+  # == Parameters:
+  # start::
+  #   starting position in the queue, defaults to current position
+  # count::
+  #   amount of items to list from the starting position, defaults to 3 items
+  #
+  # == Returns:
+  # Current queue as requested
+  get '/v1/queue' do
+    _start = params[:start]
+    _count = params[:count]
+    @queue.list_queue(_start, _count)
+  end
 
-    # GET request to `/v1/queue` will result in the current queue
-    # Optionally takes 2 arguments: start and count
-    #
-    # == Parameters:
-    # start::
-    #   starting position in the queue, defaults to current position
-    # count::
-    #   amount of items to list from the starting position, defaults to 3 items
-    #
-    # == Returns:
-    # Current queue as requested
-    get '/queue' do
-      _start = params[:start]
-      _count = params[:count]
-      @queue.list_queue(_start, _count)
+  # POST request to `/v1/queue` for adding a song to the queue
+  # Takes one argument: name
+  #
+  # == Parameters:
+  # name::
+  #   name of the song to be added
+  post '/v1/queue' do
+    _filename = params[:name]
+    if _filename.nil? or not Pathname.exist? "uploads/#{_filename}"
+      status 449
+    else
+      @queue.add _filename
     end
+  end
 
-    # POST request to `/v1/queue` for adding a song to the queue
-    # Takes one argument: name
-    #
-    # == Parameters:
-    # name::
-    #   name of the song to be added
-    post '/queue' do
-      _filename = params[:name]
-      if _filename.nil? or not Pathname.exist? "uploads/#{_filename}"
-        status 449
-      else
-        @queue.add _filename
-      end
+  # POST request to `/v1/upload` for uploading a new file to be requested.
+  # Returns error code 422 if file is not of .WAV format
+  # Returns error code 449 if no file is provided
+  # Be sure to use encoding-type in the HTML form!
+  #
+  # == Parameters:
+  # file::
+  #   Uploaded file
+  post '/v1/upload' do
+    _tempfile = params[:file][:tempfile]
+    _filename = params[:file][:filename]
+    if _tempfile.nil? or _filename.nil?
+      status 449
+    elsif _filename[-3, 4].downcase != ".wav"
+      status 422
+    else
+      FileUtils.mv _tempfile.path, "uploads/#{_filename}"
     end
+  end
 
-    # POST request to `/v1/upload` for uploading a new file to be requested.
-    # Returns error code 422 if file is not of .WAV format
-    # Returns error code 449 if no file is provided
-    # Be sure to use encoding-type in the HTML form!
-    #
-    # == Parameters:
-    # file::
-    #   Uploaded file
-    post '/upload' do
-      _tempfile = params[:file][:tempfile]
-      _filename = params[:file][:filename]
-      if _tempfile.nil? or _filename.nil?
-        status 449
-      elsif _filename[-3, 4].downcase != ".wav"
-        status 422
-      else
-        FileUtils.mv _tempfile.path, "uploads/#{_filename}"
-      end
-    end
-
-    # GET request to `/v1/search` for searching in the available files
-    # Takes one optional parameter: query
-    # Defaults to all files if no query is provided
-    #
-    # == Parameters:
-    # query::
-    #   Search query
-    #
-    # == Returns:
-    # List of queue items that match the query
-    get '/search' do
-      _query = params[:query]
-      if _query.nil?
-        Dir['./uploads']
-      else
-        Dir["./uploads/**#{_query}**"]
-      end
+  # GET request to `/v1/search` for searching in the available files
+  # Takes one optional parameter: query
+  # Defaults to all files if no query is provided
+  #
+  # == Parameters:
+  # query::
+  #   Search query
+  #
+  # == Returns:
+  # List of queue items that match the query
+  get '/v1/search' do
+    _query = params[:query]
+    if _query.nil?
+      Dir['./uploads']
+    else
+      Dir["./uploads/**#{_query}**"]
     end
   end
 end

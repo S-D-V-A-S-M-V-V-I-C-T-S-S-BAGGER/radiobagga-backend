4f3f415372e03771453daf756844b4936f9ed87e
Karel van Klink <karel@deob.us>
2019-11-21 12:31:05 +0100

    fix filepath of wav file update the install script

--- controllers/radio_controller.rb
+++ controllers/radio_controller.rb
@@ -9,7 +9,7 @@ class RadioController
   # == Returns:
   # `boolean` value that represents successful execution of the `system` function call
   def play_file(file_name)
-    _command = "sox %s -r 22050 -c 1 -b 16 -t wav - | sudo ./fm_transmitter -f %s -" % [file_name, ENV['FM_FREQUENCY']]
+    _command = "sox ./uploads/%s -r 22050 -c 1 -b 16 -t wav - | sudo ./fm_transmitter -f %s -" % [file_name, ENV['FM_FREQUENCY']]
     puts "Running: %s" % _command
     system(_command)
   end
--- install.sh
+++ install.sh
@@ -2,8 +2,14 @@
 
 echo "Resolving dependencies..."
 
-sudo apt update
-sudo apt install sox -y
+FILE=/usr/bin/sox
+if test -f "$FILE"; then
+    echo "SoX already installed, moving on..."
+else
+    sudo apt update
+    sudo apt install sox -y
+fi
+
 git clone https://github.com/markondej/fm_transmitter ./remove_me_later
 cd remove_me_later || echo "Error while cloning repository, please check your git install"
 
@@ -22,3 +28,4 @@ echo "BE SURE TO POPULATE .ENV FILE"
 echo "============================="
 
 cp .env.example .env
+mkdir uploads

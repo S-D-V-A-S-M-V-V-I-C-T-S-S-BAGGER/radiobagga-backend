7799c9f1c1b9a5acae233ed76f5bc795894ab602
Karel van Klink <karel@deob.us>
2019-11-21 13:59:02 +0100

    remove support for other file formats, should be just .wav files now

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ else
     sudo apt install sox -y
 fi
 
-git clone https://github.com/markondej/fm_transmitter ./remove_me_later
-cd remove_me_later || echo "Error while cloning repository, please check your git install"
+git clone https://github.com/markondej/fm_transmitter ./temp_clone
+cd temp_clone || echo "Error while cloning repository, please check your git install"
 
 echo "Building dependencies..."
 make
 mv ./fm_transmitter ../fm_transmitter
 cd ../
-rm -rf ./remove_me_later
+rm -rf ./temp_clone
 
 echo "Installing gem bundle"
 

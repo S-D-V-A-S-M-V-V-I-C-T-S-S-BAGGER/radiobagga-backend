0ea8b31c0d9a2c30971a869efc219f12db07cdb4
Karel van Klink <karel@deob.us>
2019-11-27 14:51:56 +0100

    display current file as radio text

--- controllers/radio_controller.rb
+++ controllers/radio_controller.rb
@@ -10,7 +10,7 @@ class RadioController
   # `boolean` value that represents successful execution of the `system` function call
   def play_file(file_name)
     # pi_fm_rds [-freq freq] [-audio file] [-ppm ppm_error] [-pi pi_code] [-ps ps_text] [-rt rt_text]
-    _command = "sox -t %s \"./uploads/%s\" -t wav - | sudo ./pi_fm_rds -freq %s -pi 6969 -ps BAGGAFM -rt \"Radio BAGGA - 'huts voor de leden\" -audio -" % [file_name[-3..-1], file_name, ENV['FM_FREQUENCY']]
+    _command = "sox -t %s \"./uploads/%s\" -t wav - | sudo ./pi_fm_rds -freq %s -pi 6969 -ps BAGGAFM -rt \"%s\" -audio -" % [file_name[-3..-1], file_name, ENV['FM_FREQUENCY'], file_name]
     puts "Running: %s" % _command
     system _command
     puts 'finished broadcast'
